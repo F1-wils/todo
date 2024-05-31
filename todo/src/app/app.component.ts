51a2bd1e1dafc716bf0d187510fcc03e44bdeac2
NNYFC <nfrnanckcesar@gmail.com>
2024-05-31 21:10:40 +0100

    Routing base implementation

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,21 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { LogoComponent } from './share/logo/logo.component';
-import { AuthenticationComponent } from './pages/authentication/authentication.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { AccountComponent } from './pages/account/account.component';
-import { HomeComponent } from './pages/home/home.component';
-import { RegistrationComponent } from './pages/registration/registration.component';
-import { SettingsComponent } from './pages/settings/settings.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 
 
 @Component({
-  imports :[ LogoComponent,DashboardComponent,AuthenticationComponent,AccountComponent,HomeComponent,RegistrationComponent,
-    SettingsComponent],
   selector: 'app-root',
   standalone: true,
-  template: '<main> <app-dashboard></app-dashboard>   </main>',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo';
