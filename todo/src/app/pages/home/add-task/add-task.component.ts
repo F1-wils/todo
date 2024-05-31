a78693d6ae482045ce9bc09f0b67b9b7c9ecd7de
NNYFC <nfrnanckcesar@gmail.com>
2024-05-31 14:33:40 +0100

    dashboard and home added

---
+++ todo/src/app/pages/home/add-task/add-task.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      add-task works!
+    </p>
+  `,
+  styleUrl: './add-task.component.css'
+})
+export class AddTaskComponent {
+
+}
---
+++ todo/src/app/pages/home/task-detail/task-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-detail',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      task-detail works!
+    </p>
+  `,
+  styleUrl: './task-detail.component.css'
+})
+export class TaskDetailComponent {
+
+}
