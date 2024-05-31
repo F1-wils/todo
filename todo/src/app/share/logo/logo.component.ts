15f8b70bfc8ab82b9a94efbf9e02ff82343f8093
F1-wils <ebanje1enang2@gmail.com>
2024-05-31 17:24:17 +0100

    logo done

--- todo/src/app/share/logo/logo.component.ts
+++ todo/src/app/share/logo/logo.component.ts
@@ -5,9 +5,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      logo works!
-    </p>
+    <section>
+    <header class="brand-name">
+      <div style="  width:300px;
+  margin-top: 1.5cm; margin-left: 1cm;">
+      <a href="url" style="color: black; font-size:1.6cm;font-weight: bolder; text-decoration: none;">TO-DO</a>
+ 
+      </div>
+     </header>
+   </section>
   `,
   styleUrl: './logo.component.css'
 })
